69d07a999ea772f992c031863260beb090eaf5b5
Brunno321 <brunnoporto@gmail.com>
2026-08-18 14:01:33 -0300

    update css

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'block_mwa_dashboard';
-$plugin->version = 2026081622;
+$plugin->version = 2026081802;
 $plugin->requires     = 2022112800; // Moodle 4.1+
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->release   = '3.8';
